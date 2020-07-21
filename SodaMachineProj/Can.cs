4716dab1636f2a74972ce45015ca9997df38cf4c
Work & School <chouac@live.com>
2020-07-21 10:30:15 -0500

    Classes have been created and simple (member variables, constructors, methods) template have been added for eac class.

---
+++ SodaMachineProj/Can.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SodaMachineProj
+{
+    class Can
+    {
+        //Member Variables (Has A)
+
+        //Constructor (Spawner)
+
+        //Member Methods (Can Do)
+    }
+}
---
+++ SodaMachineProj/Wallet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SodaMachineProj
+{
+    class Wallet
+    {
+        //Member Variables (Has A)
+
+        //Constructor (Spawner)
+
+        //Member Methods (Can Do)
+    }
+}
